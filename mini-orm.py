89fbe96d7dfe9456823909abb742a015f7f6f70b
Dmitry Morozov <9984398@gmail.com>
2024-12-21 18:48:47 +0300

    --> Class ORM : Added

---
+++ mini-orm.py
@@ -0,0 +1,21 @@
+class ORM:
+    def __init__(self):
+        pass
+
+    def insert_item(self):
+        pass
+
+    def select_item(self):
+        pass
+
+    def update_item(self):
+        pass
+
+    def delete_item(self):
+        pass
+
+    def fetch_one_item(self):
+        pass
+
+    def fetch_all_items(self):
+        pass
