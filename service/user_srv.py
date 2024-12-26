fe31140c4539ae4a4324688e5bf3898f3ef537d5
Dmitry Morozov <9984398@gmail.com>
2024-12-26 16:39:51 +0300

    --> User services : Update

--- service/user_srv.py
+++ service/user_srv.py
@@ -1,28 +1,80 @@
-import sqlite3
+from lazy_orm.db_manager import DatabaseManager, DatabaseError
+from utils.email import validate_and_normalize_email
 
-from email_validator import EmailNotValidError
+import logging
 
-from mini_orm import ORM
-from models.models import User
-from utils.utils import is_email_valid
+USERS_TABLE = 'users'
+USERS_COLUMNS = ['id', 'email', 'username', 'phone', 'age']
 
 
-def add_user(orm: ORM, user: User):
+def is_user_exists(db_manager: DatabaseManager, username: str) -> bool:
+    """
+    :param db_manager: The database manager instance used to interact with the database.
+    :param username: The username of the user to check for existence in the database.
+    :return: A boolean value indicating whether the user exists (True) or not (False).
+    """
+    users = db_manager.fetch_if('users', f'username="{username}"')
+    return len(users) > 0
+
+
+def log_user_addition(username: str, email: str) -> None:
+    """
+    :param username: The username of the new user being added.
+    :param email: The email address of the new user being added.
+    :return: None
+    """
+    logging.info(f'New User {username} with email: {email} added.')
+
+
+def add_user(orm: DatabaseManager, username: str, email: str, age: int) -> bool:
+    """
+    :param orm: Instance of the DatabaseManager used to interact with the database.
+    :param username: The username of the user to be added to the database.
+    :param email: The email address of the user to be added; it will be validated and normalized before insertion.
+    :param age: The age of the user to be added to the database.
+    :return: Returns True if the user is successfully added, otherwise returns False due to errors.
+    """
     try:
-        correct_email = is_email_valid(user.email)
-        user.email = correct_email
-        user_data = vars(user)  # converts user object to dict
-        orm.insert_item('users', user_data)
-    except EmailNotValidError as e:
-        print(e)
+        column_values = {
+            'username': username,
+            'email': validate_and_normalize_email(email),
+            'age': age,
+        }
+        orm.insert('users', column_values)
+        log_user_addition(username, email)
+        return True
+    except DatabaseError as e:
+        logging.exception(f"Error adding user: {e}")
+        return False
 
-def get_all_users(orm: ORM, table_name: str):
-    query = f'SELECT * FROM {table_name};'
+
+async def add_admin_user(db_manager):
     try:
-        cursor = orm.get_cursor()
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        for row in rows:
-            print(f'User: {row}')
-    except sqlite3.Error as e:
-        print(f'An error occurred: {e}')
+        column_values = {
+            'username': 'Admin',
+            'email': '9984398@gmail.com',
+            'age': 100,
+            'phone': '+79219984444',
+        }
+        db_manager.insert('users', column_values)
+        logging.info('Admin user added successfully.')
+    except DatabaseError as e:
+        logging.exception(f"Error adding admin user: {e}")
+
+
+async def handle_empty_users(db_manager: DatabaseManager):
+    """Handles the scenario when no products are present in the database."""
+    await add_admin_user(db_manager)
+    logging.info('No users found. Admin User have been added.')
+
+
+async def get_all_users(db_manager: DatabaseManager):
+    try:
+        users = await db_manager.fetch_all(USERS_TABLE)
+        if not users:
+            await handle_empty_users(db_manager)
+            users = await db_manager.fetch_all(USERS_TABLE, USERS_COLUMNS)
+        return users
+    except DatabaseError as e:
+        logging.exception(f"Error fetching users: {e}")
+        return []
