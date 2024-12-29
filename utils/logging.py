94bafcb757b8e3d54b15aadc689f0df73e50f633
Aleks L <alekslk@pm.me>
2024-12-29 13:18:01 +0100

    refactoring

--- utils/logging.py
+++ utils/logging.py
@@ -1,6 +1,8 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+LOG_NAME = 'simple_inventory.log'
+
 
 class LogColors:
     """ANSI escape sequences for colors"""
@@ -27,6 +29,13 @@ class ColorFormatter(logging.Formatter):
         return super().format(record)
 
 
+def get_formatter():
+    """Returns the logging format and date format."""
+    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    date_format = '%Y-%m-%d %H:%M:%S'
+    return log_format, date_format
+
+
 def setup_logging():
     """ Configures the logging system for the application.
      Sets up the root logger to log messages to both a rotating file and the console with a specific format.
@@ -36,21 +45,16 @@ def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    # Log message format
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
+    log_format, date_format = get_formatter()
+    file_formatter = logging.Formatter(log_format, date_format)
+    console_formatter = ColorFormatter(log_format, date_format)
 
     # File handler with log rotation
-    file_handler = RotatingFileHandler('app.log', maxBytes=10_000_000, backupCount=5)
-    file_handler.setFormatter(formatter)
+    file_handler = RotatingFileHandler(LOG_NAME, maxBytes=10_000_000, backupCount=5)
+    file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
 
     # Console handler
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(ColorFormatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    ))
+    console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
