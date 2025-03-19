79b875d6619f8aecc331a687f16074661076f0d4
Dmitry Morozov <9984398@gmail.com>
2025-03-19 13:21:48 +0300

    --> Deep Refactored #1

--- lazy_orm/db_manager.py
+++ lazy_orm/db_manager.py
@@ -1,10 +1,18 @@
 import os
 import sqlite3
 from typing import Any, Dict, List, Optional
-
 import logging
 
-logging.getLogger().setLevel(logging.INFO)
+
+# Constants for default paths and file names
+DEFAULT_DB_DIR = 'data'
+DEFAULT_SQL_DIR = 'sql'
+CREATE_DB_SCRIPT_TEMPLATE = 'create_{db_name}_db.sql'
+SQLITE_MASTER_TABLE = 'sqlite_master'
+TABLE_TYPE = 'table'
+
+# General Placeholder Constants
+WILDCARD_COLUMNS = '*'
 
 
 class DatabaseError(Exception):
@@ -13,8 +21,8 @@ class DatabaseError(Exception):
 
     This exception is raised when there is an issue related to database operations or interactions.
 
-    :param message: A descriptive error message providing details about the database error.
-    :type message: str
+    Attributes:
+        message (str): A descriptive error message providing details about the database error.
     """
 
     def __init__(self, message: str) -> None:
@@ -23,10 +31,13 @@ class DatabaseError(Exception):
 
 class DatabaseManager:
     """
-    A class to manage SQLite database operations, including creating, reading, updating, and deleting records, as well as initializing and checking database existence.
+    A class to manage SQLite database operations.
+
+    Handles database creation, reading, updating, and deletion operations. It also verifies the existence of required
+    tables and initializes them if necessary.
     """
 
-    def __init__(self, db_name: str, db_dir: str = 'data') -> None:
+    def __init__(self, db_name: str, db_dir: str = DEFAULT_DB_DIR) -> None:
         """
         Initializes the DatabaseManager with specified database name and directory.
 
@@ -34,17 +45,17 @@ class DatabaseManager:
             db_name (str): The name of the SQLite database file.
             db_dir (str): The directory where the database file is located.
         """
-        self.db_path: str = os.path.join(db_dir, db_name)
-        self.conn: sqlite3.Connection = self._connect_to_db()
-        self.__cursor: sqlite3.Cursor = self.conn.cursor()
-        self.__db_name = db_name
-        self._check_db_exists()
+        self.db_name = db_name
+        self.db_path: str = os.path.join(db_dir, db_name)  # Full path to the database file
+        self.conn: sqlite3.Connection = self._connect_to_db()  # Initialize database connection
+        self.cursor: sqlite3.Cursor = self.conn.cursor()  # Initialize the database cursor
+        self._check_db_exists()  # Verify if the database exists
 
     def __del__(self) -> None:
         """Destructor to close the SQLite connection."""
         if self.conn:
+            logging.info('Closing database connection.')
             self.conn.close()
-            logging.info('Connection closed successfully.')
 
     def _connect_to_db(self) -> sqlite3.Connection:
         """
@@ -54,27 +65,15 @@ class DatabaseManager:
             sqlite3.Connection: The SQLite connection object.
         """
         try:
+            logging.info(f"Connecting to database at {self.db_path}...")
             return sqlite3.connect(self.db_path)
         except sqlite3.OperationalError as e:
-            logging.exception(f'Operational Error {e} when trying ti connect ro {self.db_path}')
+            logging.exception(f"SQLite Operational Error: {e}. Attempting to create directory for the database.")
             os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
             return sqlite3.connect(self.db_path)
         except Exception as e:
-            raise DatabaseError(f"Failed to connect to the database: {e}")
-
-    @staticmethod
-    def _row_to_dict(row: tuple, columns: List[str]) -> Dict[str, Any]:
-        """
-        Converts a row tuple to a dictionary mapping column names to their values.
-
-        Args:
-            row (tuple): A tuple representing a row of data.
-            columns (List[str]): A list of column names corresponding to the tuple values.
-
-        Returns:
-            Dict[str, Any]: A dictionary where keys are column names and values are the corresponding row values.
-        """
-        return {column: row[idx] for idx, column in enumerate(columns)}
+            logging.exception("Unexpected error occurred while connecting to the database.")
+            raise DatabaseError(f"Failed to connect to the database: {str(e)}")
 
     def insert(self, table: str, column_values: Dict[str, Any]) -> None:
         """
@@ -90,9 +89,12 @@ class DatabaseManager:
 
         try:
             query = f'INSERT INTO {table} ({columns}) VALUES ({placeholders})'
-            self.__cursor.execute(query, values)
+            logging.info(f"Executing query: {query} with values: {values}")
+            self.cursor.execute(query, values)
             self.conn.commit()
+            logging.info(f"Successfully inserted into table '{table}'.")
         except sqlite3.Error as e:
+            logging.exception("Insert operation failed.")
             raise DatabaseError(f"Insert operation failed: {e.args[0]}")
 
     async def fetch_all(self, table: str, columns: List[str]) -> List[Dict[str, Any]]:
@@ -109,32 +111,38 @@ class DatabaseManager:
         columns_str = ", ".join(columns)
         try:
             query = f'SELECT {columns_str} FROM {table}'
-            self.__cursor.execute(query)
-            rows = self.__cursor.fetchall()
+            logging.info(f"Executing query: {query}")
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            logging.info(f"Successfully fetched {len(rows)} rows from table '{table}'.")
             return [self._row_to_dict(row, columns) for row in rows]
         except sqlite3.Error as e:
+            logging.exception("Fetch operation failed.")
             raise DatabaseError(f"Fetch operation failed: {e.args[0]}")
 
     def fetch_if(self, table: str, condition: str, columns: Optional[List[str]] = None) -> List[Dict[str, Any]]:
         """
-        Fetches all rows from the specified table, where condition is True with given columns.
+        Fetches rows from the specified table based on a condition.
 
         Args:
             table (str): The table name.
-            condition (str): The condition for fetching rows.
+            condition (str): The condition for fetching rows (e.g., "id > 10").
             columns (List[str], optional): A list of column names to fetch. Defaults to '*'.
 
         Returns:
             List[Dict[str, Any]]: A list of dictionaries representing the fetched rows.
         """
-        columns_str = '*' if columns is None else ', '.join(columns)
+        columns_str = WILDCARD_COLUMNS if columns is None else ', '.join(columns)
         try:
             query = f"SELECT {columns_str} FROM {table} WHERE {condition}"
-            self.__cursor.execute(query)
-            rows = self.__cursor.fetchall()
-            col_names = [desc[0] for desc in self.__cursor.description]
+            logging.info(f"Executing query: {query}")
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            col_names = [desc[0] for desc in self.cursor.description]
+            logging.info(f"Successfully fetched {len(rows)} rows from table '{table}' with condition '{condition}'.")
             return [self._row_to_dict(row, col_names) for row in rows]
         except sqlite3.Error as e:
+            logging.exception("Fetch with condition operation failed.")
             raise DatabaseError(f"Fetch operation with condition failed: {e.args[0]}")
 
     def delete(self, table: str, row_id: int) -> None:
@@ -147,20 +155,13 @@ class DatabaseManager:
         """
         try:
             query = f'DELETE FROM {table} WHERE id = ?'
-            self.__cursor.execute(query, (row_id,))
+            logging.info(f"Executing query: {query} with ID = {row_id}")
+            self.cursor.execute(query, (row_id,))
             self.conn.commit()
-            logging.info(f'Deletion of the user with ID = {row_id} is complete')
+            logging.info(f"Successfully deleted row with ID = {row_id} from table '{table}'.")
         except sqlite3.Error as e:
+            logging.exception("Delete operation failed.")
             raise DatabaseError(f"Delete operation failed: {e.args[0]}")
-
-    def get_cursor(self) -> sqlite3.Cursor:
-        """
-        Returns the cursor object.
-
-        Returns:
-            sqlite3.Cursor: The cursor object.
-        """
-        return self.__cursor
 
     def update(self, table: str, column_values: Dict[str, Any], condition: str) -> None:
         """
@@ -176,9 +177,12 @@ class DatabaseManager:
         query = f"UPDATE {table} SET {columns} WHERE {condition}"
 
         try:
-            self.__cursor.execute(query, values)
+            logging.info(f"Executing query: {query} with values: {values}")
+            self.cursor.execute(query, values)
             self.conn.commit()
+            logging.info(f"Successfully updated rows in table '{table}' with condition '{condition}'.")
         except sqlite3.Error as e:
+            logging.exception("Update operation failed.")
             raise DatabaseError(f"Update operation failed: {e.args[0]}")
 
     def get_table_size(self, table: str) -> int:
@@ -189,52 +193,65 @@ class DatabaseManager:
             table (str): The table name.
 
         Returns:
-            int: The total number of rows.
+            int: The total number of rows in the table.
         """
         try:
             query = f'SELECT COUNT(*) FROM {table}'
-            self.__cursor.execute(query)
-            return self.__cursor.fetchone()[0]
+            logging.info(f"Executing query: {query}")
+            self.cursor.execute(query)
+            count = self.cursor.fetchone()[0]
+            logging.info(f"Table '{table}' contains {count} rows.")
+            return count
         except sqlite3.Error as e:
+            logging.exception("Get table size operation failed.")
             raise DatabaseError(f"Get table size operation failed: {e.args[0]}")
 
-    def _init_db(self, start_path=None) -> None:
+    def _init_db(self, start_path: Optional[str] = None) -> None:
         """
-        Initializes the database by executing SQL commands from 'create_users_db.sql' file.
+        Initializes the database by executing SQL commands from the schema file.
         """
         try:
-            logging.info(f'Current Path: {os.getcwd()}')
-
-            if start_path is None:
-                start_path = 'sql'
-
-            generic_creator_file = os.path.join(start_path, f'create_{self.__db_name}_db.sql')
-            logging.info(f'Generic Creator File: {generic_creator_file}')
-
-            with open(os.path.join(generic_creator_file)) as fd:
+            logging.info("Initializing database...")
+            start_path = start_path or DEFAULT_SQL_DIR
+            schema_file = os.path.join(start_path, CREATE_DB_SCRIPT_TEMPLATE.format(db_name=self.db_name))
+            logging.info(f"Reading schema file: {schema_file}")
+            with open(schema_file) as fd:
                 sql = fd.read()
-
-            self.__cursor.executescript(sql)
+            self.cursor.executescript(sql)
             self.conn.commit()
-
-            logging.info(f'Database {self.__db_name} initialized successfully!')
+            logging.info(f"Database '{self.db_name}' initialized successfully!")
         except (FileNotFoundError, sqlite3.Error) as e:
-            raise DatabaseError(f'Database initialization failed: {e}')
+            logging.exception("Database initialization failed.")
+            raise DatabaseError(f"Database initialization failed: {e}")
 
     def _check_db_exists(self) -> None:
         """
-        Checks if the required tables exist in the database, and initializes the database if not.
+        Checks if the required database tables exist, and initializes the database if they do not.
         """
         try:
-
-            query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.__db_name}'"
-            self.__cursor.execute(query)
-
-            table_exists = self.__cursor.fetchall()
+            query = f"SELECT name FROM {SQLITE_MASTER_TABLE} WHERE type='{TABLE_TYPE}' AND name='{self.db_name}'"
+            logging.info(f"Executing query: {query}")
+            self.cursor.execute(query)
+            table_exists = self.cursor.fetchone()
             if not table_exists:
-                logging.warning(f'Table {self.__db_name} does not exist! ')
+                logging.warning(f"Table '{self.db_name}' does not exist. Initializing database...")
                 self._init_db()
             else:
-                logging.info(f'Database {self.db_path} exists and checked!')
+                logging.info(f"Database '{self.db_name}' exists and is ready.")
         except sqlite3.Error as e:
+            logging.exception("Check database existence operation failed.")
             raise DatabaseError(f"Check database existence operation failed: {e.args[0]}")
+
+    @staticmethod
+    def _row_to_dict(row: tuple, columns: List[str]) -> Dict[str, Any]:
+        """
+        Converts a row tuple to a dictionary mapping column names to their values.
+
+        Args:
+            row (tuple): A tuple representing a row of data.
+            columns (List[str]): A list of column names corresponding to the tuple values.
+
+        Returns:
+            Dict[str, Any]: A dictionary where keys are column names and values are the corresponding row values.
+        """
+        return {column: row[idx] for idx, column in enumerate(columns)}
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import asyncio
-import sys
-import time
-
+import logging
 from lazy_orm.db_manager import DatabaseManager
 from service.user_srv import get_all_users, add_user
-
 from utils.logging_simp_inv import setup_logging
 
 USERS_DB_NAME = 'users'
+SAMPLE_USERS = [
+    {'username': 'Arina5', 'email': 'Arisha5@librem.com', 'age': 20},
+    {'username': 'Alex', 'email': 'something@gmail.com', 'age': 40},
+    {'username': 'Dmitry', 'email': 'morozovd@yandex.ru', 'age': 18}
+]
+
+
+async def _add_sample_users(db_manager):
+    """Add sample users to the database."""
+    for user in SAMPLE_USERS:
+        result = add_user(db_manager, user['username'], user['email'], user['age'])
+        logging.info(result) if result else logging.error('Error adding new User!')
 
 
 async def main():
     db_manager = DatabaseManager(USERS_DB_NAME)
-
-    add_user(db_manager, 'Arina5', 'Arisha5@librem.com', 20)
-    add_user(db_manager, 'Alex', 'something@gmail.com', 40)
-    add_user(db_manager, 'Dmitry', 'morozovd@yandex.ru', 18)
-
+    await _add_sample_users(db_manager)
     users = await get_all_users(db_manager)
-
     for user in users:
         print(user)
 
