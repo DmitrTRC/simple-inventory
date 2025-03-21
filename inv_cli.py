a0000f6275324b8c0f9b7386c3a4d3616b4133ac
Dmitry Morozov <9984398@gmail.com>
2025-03-22 01:13:10 +0300

    ->Draft implementation: Done

--- inv_cli.py
+++ inv_cli.py
@@ -1,22 +1,33 @@
+from unicodedata import category
+
 import typer
 from rich.console import Console
 from rich.table import Table
+
+from model.todo_model import Todo, Category
 
 console = Console()
 
 app = typer.Typer()
 
 todos = [
-    "Buy groceries",
-    "Complete Python project",
-    "Go for a run"
+    Todo('Buy groceries'),
+    Todo('Complete Python project'),
+    Todo('Go for a run')
 ]
 
 
 @app.command('new', short_help='Create a new task')
-def create_task(name: str):
-    todos.append(name)
+def create_task(name: str, cat: str = 'BACKLOG'):
+    if cat.upper() not in Category.__members__:
+        console.print(
+            f"[red]Error: '{cat}' is not a valid category. Valid options are: {', '.join(Category.__members__.keys())}[/red]")
+        return
+
+    cat = Category[cat.upper()]
+    todos.append(Todo(name, category=cat))
     console.print(f"Task '{name}' created successfully!")
+    list_tasks()
 
 
 @app.command('list', short_help='List all tasks')
@@ -24,8 +35,22 @@ def list_tasks():
     table = Table(title="To-Do List")
     table.add_column("ID", justify="right", style="cyan", no_wrap=True)
     table.add_column("Task", style="magenta")
-    for idx, task in enumerate(todos, 1):
-        table.add_row(str(idx), task)
+    table.add_column("Category", style="green")
+    table.add_column("Date Added", style="yellow")
+    table.add_column("Date Completed", style="blue")
+    table.add_column("Status", style="red")
+
+    for todo_item in todos:
+        table.add_row(
+            str(todo_item.position),
+            todo_item.task,
+            str(todo_item.category.name),
+            str(todo_item.date_added) if todo_item.date_added else "N/A",
+            str(todo_item.date_completed) if todo_item.date_completed else "N/A",
+            str(todo_item.status.name),
+
+        )
+
     console.print(table)
 
 
@@ -34,6 +59,7 @@ def delete_task(task_id: int):
     if 0 < task_id <= len(todos):
         removed_task = todos.pop(task_id - 1)
         console.print(f"Task '{removed_task}' deleted successfully!")
+        list_tasks()
     else:
         console.print("Invalid task ID!")
 
@@ -41,8 +67,9 @@ def delete_task(task_id: int):
 @app.command('update', short_help='Update a task by ID')
 def update_task(task_id: int, new_name: str):
     if 0 < task_id <= len(todos):
-        todos[task_id - 1] = new_name
+        todos[task_id - 1].task = new_name
         console.print(f"Task #{task_id} updated to '{new_name}' successfully!")
+        list_tasks()
     else:
         console.print("Invalid task ID!")
 
