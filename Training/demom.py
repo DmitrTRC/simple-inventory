21a49b019257d23a3a64370c9ecc7f7b9347f582
Dmitry Morozov <9984398@gmail.com>
2025-03-25 21:51:34 +0300

    ->Home Task

---
+++ Training/demom.py
@@ -0,0 +1,4 @@
+from module.MyClass import my_func
+
+print ( 'Hello World')
+my_func()
---
+++ Training/module/MyClass.py
@@ -0,0 +1,14 @@
+def my_func():
+    print("Hello from my_func()")
+
+
+def my_func2():
+    print("Hello from my_func2()")
+
+
+if __name__ == '__main__':
+    my_func()
+    my_func2()
+
+
+
